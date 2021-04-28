987e61172a17c989c7a95887acd48dd6b7caf7fe
JosueDa <morales181101@unis.edu.gt>
2021-04-27 23:01:58 -0600

    Add JenkinsFile.groovy

---
+++ JenkinsFile.groovy
@@ -0,0 +1,41 @@
+def checkout = { String subFolder, String repo ->
+    checkout([
+            $class      : "GitSCM",
+            branches    : [[name: "*/master"]],
+            extensions  : [[ $class: "RelativeTargetDirectory",
+                             relativeTargetDir: subFolder
+                           ]],
+            userRemoteConfigs: [[
+                                        url : repo,
+                                        credentialsId: "jenkins"
+                                ]]
+    ])
+}
+
+def runner = { commandToExecute -> isUnix() ? sh(commandToExecute) : bat(commandToExecute) }
+
+
+unitTest:{
+    stage("Unit Testing"){
+        node('NewNode'){
+            checkout("tests","git@github.com:ccx54392/corecodeproject_api_.git" )
+            runner 'mvn test -DincludeGroup=unitTesting'
+        }
+    }
+}
+IntegrationTest:{
+    stage("Integration Testing"){
+        node('NewNode'){
+            checkout("tests","git@github.com:JosueDa/corecodeproject_testing.git")
+            runner 'mvn test -Dgroups=integrationTest'
+        }
+    }
+}
+SystemTest:{
+    stage("System or E2E Testing"){
+        node('NewNode'){
+            checkout("tests","git@github.com:JosueDa/corecodeproject_testing.git")
+            runner 'mvn test -Dgroups=systemTest'
+        }
+    }
+}
